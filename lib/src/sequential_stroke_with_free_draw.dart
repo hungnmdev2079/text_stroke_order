652e55e6d886dde31a4c924a4d4dd0d89eb21d55
dangvantien <dangvantien.mochi@gmail.com>
2024-07-10 16:04:01 +0700

    china-tiendv | fix

--- lib/src/sequential_stroke_with_free_draw.dart
+++ lib/src/sequential_stroke_with_free_draw.dart
@@ -65,7 +65,7 @@ class _SequentialStrokeWithFreeDrawState
     super.initState();
     widget.controller.animationController.forward();
     if (widget.randomSkipTutorial) {
-      widget.controller.setRandomSkipStrokeOrder();
+      widget.controller.setSkipStrokeOrder();
     }
     // widget.controller.updateAnimateStrokeColor(widget.animatingStrokeColor);
     widget.controller.updateTutorial();
